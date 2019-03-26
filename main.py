080b3a44c0397b3570f8bfabf5bc609c4b44aaef
paltenir <48948843+paltenir@users.noreply.github.com>
2019-03-26 18:54:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,19 +5,18 @@ opt = hysysopt.init()
 
 opt.get_params('opt')
 
-opt.optimize_feed_location("C-3501", "Solvent", lb_frac=0, ub_frac=0.7)
-opt.optimize_feed_location("C-3501", "Heated Feed", lb_frac=0.3, ub_frac=1)
+opt.optimize_feed_location("Your column name", "Your feed stream name", lb_frac=0, ub_frac=1)
 
 opt.list_params()
 
 opt.attach_cost_function(column_cost_function, optype="columns")
 opt.attach_cost_function(hx_cost_function, optype="heatexchangers")
 
-opt.set_column_efficiency('C-3501', 0.62240826)
+opt.set_column_efficiency("Your column name", 0.7)
 
 opt.display_convergence = True
-opt.set_save_location('actual_1.csv')
-opt.run(n_iter=20, num=40, save_data=True)
+opt.set_save_location('saved_data.csv')
+opt.run(n_iter=20, num=20, save_data=True)
 
 opt.set_to_optimal()
 opt.show_individual_costs()
